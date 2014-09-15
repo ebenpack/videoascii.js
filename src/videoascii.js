2bbad9103696f96cb9ae71eaa7514ce137739b4b
ebenpack <ebenpackwood@gmail.com>
2014-09-15 16:55:02 -0400

    Cleanup

--- src/videoascii.js
+++ src/videoascii.js
@@ -8,33 +8,34 @@ function videoascii(options){
     var font_size = (options.font_size === undefined) ? 12 : options.font_size;
     var monochrome = (options.monochrome === undefined) ? true : options.monochrome;
 
-    var width, height, image_data, ascii_data, output_char_height,
-        output_char_width, ascii_array, aspect_ratio, output_height,
-        input_block_width, input_block_height, output_block_width, output_block_height,
-        font_width, font_height;
+    var width, height, image_data, aspect_ratio,
+        output_height, font_width, font_height;
 
+    // Video frames are drawn to offscreen buffer canvas
+    // in order to get pixel data into ImageData array.
     var buffer_canvas = document.createElement('canvas');
     var buffer_ctx = buffer_canvas.getContext('2d');
 
-    // Initialization
-    // Given output width, and output 'resolution', calculate block size(?)
 
     video.addEventListener('loadeddata', function(){
-        ctx.font = font_size + "pt Courier";
+        // Setting width/height resets the context, so these go first.
         width = Math.floor(video.getBoundingClientRect().width);
         height = Math.floor(video.getBoundingClientRect().height);
-        output_width = (output_width === undefined) ? width : output_width;
-        aspect_ratio = width / height;
-        output_height = Math.floor(output_width / aspect_ratio);
-       
-        canvas.width = output_width;
-        canvas.height = output_height;
         buffer_canvas.width = width;
         buffer_canvas.height = height;
-        image_data = buffer_ctx.getImageData(0,0,width,height);
-        ascii_data = buffer_ctx.getImageData(0,0,width,height);
-        output_char_height = Math.floor(output_height / font_height);
-        output_char_width = Math.floor(output_width / font_width);
+
+        aspect_ratio = width / height;
+        // Set output canvas to same aspect ratio as video
+        // If no output width is specified, output canvas will
+        // be same size as video.
+        output_width = (output_width === undefined) ? width : output_width;
+        output_height = Math.floor(output_width / aspect_ratio);
+        canvas.width = output_width;
+        canvas.height = output_height;
+
+        ctx.font = font_size + "pt Courier";
+       
+        image_data = buffer_ctx.getImageData(0, 0, width, height);
         window.requestAnimationFrame(update);
     });
 
@@ -44,10 +45,9 @@ function videoascii(options){
             window.requestAnimationFrame(update);
             return;
         }
-        buffer_ctx.drawImage(video,0,0);
-        image_data = buffer_ctx.getImageData(0,0,width,height);
+        buffer_ctx.drawImage(video, 0, 0);
+        image_data = buffer_ctx.getImageData(0, 0, width, height);
         ctx.clearRect(0, 0, output_width, output_height);
-        ctx.font = font_size + "pt Courier";
         asciify(image_data, canvas, font_size, monochrome);
         window.requestAnimationFrame(update);
     }
